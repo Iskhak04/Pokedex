e6002f24a4fdab496ad13f02d9ca6fd45f3132fc
Iskhak <izhtnv04@gmail.com>
2023-06-15 17:35:58 +0600

    initial commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Iskhak Zhutanov on 15.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
